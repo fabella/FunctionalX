2397cad1920fc63f5e4b2d35cd71b5ff5cda2584
Fidel Abella <fabella42@gmail.com>
2016-07-13 16:47:35 -0500

    Added string extentions to convert from strings to numbers and also date time. Added Unit test as well. Also, added some useful functions from haskell on Maybe type

---
+++ test/FunctionalX.Tests/FunctionalTests.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using NUnit.Framework;
+
+namespace FunctionalX.Tests
+{
+    using static Functional;
+
+    [TestFixture]
+    public class FunctionalTests
+    {
+        [TestCase("hello", ExpectedResult = "hello")]
+        public string FromJustOnJustReturnsValue(string maybe)
+            => FromJust(Just(maybe));
+
+        [TestCase(null, ExpectedResult = true)]
+        public bool FromJustOnNothingThrowsInvalidOperationException(string val)
+        {
+            try
+            {
+                FromJust(Just<string>(val));
+            }
+            catch (InvalidOperationException e)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        [TestCase("x", "y", ExpectedResult = "x")]
+        [TestCase(null, "y", ExpectedResult = "y")]
+        [TestCase(null, null, ExpectedResult = null)]
+        public string FromMaybeTest(string s, string val)
+            => FromMaybe(Just(s), val);
+
+        [TestCase("x", "y", ExpectedResult = "x")]
+        [TestCase(null, "y", ExpectedResult = "y")]
+        [TestCase(null, null, ExpectedResult = null)]
+        public string FromMaybeFuncTest(string s, string val)
+            => FromMaybe(Just(s), () => val);
+
+        [Test]
+        public void ListToMaybeOnEmptyList()
+        {
+            var empty = new List<int>();
+
+            var sut = empty.ListToMaybe();
+            Assert.IsTrue(Nothing == sut);
+        }
+
+        [Test]
+        public void ListToMaybeOnNonEmptyList()
+        {
+            var empty = new List<int>() { 1, 2, 3};
+
+            var sut = empty.ListToMaybe();
+            Assert.AreEqual(Just(1), sut);
+        }
+
+        [Test]
+        public void MaybeToListOnNothing()
+        {
+            var x = Just<string>(null);
+            var sut = MaybeToList(x);
+
+            Assert.IsTrue(!sut.Any());
+        }
+
+        [Test]
+        public void MaybeToListOnJust()
+        {
+            var x = Just<string>("hi");
+            var sut = MaybeToList(x);
+
+            Assert.IsTrue(sut.Any());
+        }
+
+        [Test]
+        public void CatMaybesTest()
+        {
+            var xs = new List<Maybe<string>>() {"x", "y", "z", null, null};
+            var sut = xs.CatMaybes();
+            Assert.AreEqual(3, sut.Count());
+        }
+
+        [Test]
+        public void CatMaybesTestOnNothings()
+        {
+            var xs = new List<Maybe<string>>() {null, null, null};
+            var sut = xs.CatMaybes();
+            Assert.IsFalse(sut.Any());
+        }
+
+        [Test]
+        public void MapMaybesTest()
+        {
+            var xs = new List<string>() {"x", "y", "z", null, null};
+            var sut = xs.MapMaybe(x => x == null ? Nothing : Just(x.ToUpper()));
+            Assert.AreEqual(3, sut.Count());
+            Assert.AreEqual(new List<string>() { "X", "Y", "Z"}, sut);
+            
+        }
+    }
+}
---
+++ test/FunctionalX.Tests/StringExtensionsTests.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+
+namespace FunctionalX.Tests
+{
+    using static Functional;
+    using static Maybe;
+    [TestFixture]
+    public class StringExtensionsTests
+    {
+        [TestCase("42", ExpectedResult = 42)]
+        [TestCase("not_an_int", ExpectedResult = null)]
+        public int? TestParseInt(string s)
+            => s.ParseInt().IsJust ? FromJust(s.ParseInt()) : (int?)null;
+
+        [TestCase("42", 0, ExpectedResult = 42)]
+        [TestCase("not_an_int", 42, ExpectedResult = 42)]
+        public int TestParseInt(string s, int @default)
+            => s.ParseInt(@default);
+
+        [TestCase("42.0", ExpectedResult = 42.0)]
+        [TestCase("not_a_double", ExpectedResult = null)]
+        public double? TestParseDouble(string s)
+            => s.ParseDouble().IsJust ? FromJust(s.ParseDouble()) : (double?)null;
+
+        [TestCase("42.0", 0, ExpectedResult = 42.0)]
+        [TestCase("not_a_double", 42.0, ExpectedResult = 42.0)]
+        public double TestParseDouble(string s, double @default)
+            => s.ParseDouble(@default);
+
+        [TestCase("42.0", ExpectedResult = 42.0)]
+        [TestCase("not_a_float", ExpectedResult = null)]
+        public float? TestParseFloat(string s)
+            => s.ParseFloat().IsJust ? FromJust(s.ParseFloat()) : (float?)null;
+
+        [TestCase("42.0", 0, ExpectedResult = 42.0)]
+        [TestCase("not_a_float", (float)42.0, ExpectedResult = 42.0)]
+        public float TestParseFloat(string s, float @default)
+            => s.ParseFloat(@default);
+
+        [TestCase("42.0", ExpectedResult = 42.0)]
+        [TestCase("not_a_decimal", ExpectedResult = null)]
+        public decimal? TestParseDecimal(string s)
+            => s.ParseDecimal().IsJust ? FromJust(s.ParseDecimal()) : (decimal?)null;
+
+        [TestCase("42.0", 0, ExpectedResult = 42.0)]
+        [TestCase("not_a_decimal", 42.0, ExpectedResult = 42.0)]
+        public decimal TestParseDecimal(string s, decimal @default)
+            => s.ParseDecimal(@default);
+
+        [TestCase("not_a_date", ExpectedResult = false)]
+        [TestCase("01/01/2016", ExpectedResult = true)]
+        public bool TestParseDate(string s)
+            => s.ParseDate().IsJust;
+    }
+}
